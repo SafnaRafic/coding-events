b95d09980c02de3e70ea6ebea648691901137edb
Safna <safna.usa@gmail.com>
2019-12-16 00:23:22 -0600

    model package and event class added

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +15,23 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events=new ArrayList<>();
+    private static List<Event> events=new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model){
         model.addAttribute("events",events);
         return "events/index";
     }
+
     //lives at /events/create
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
     }
+
     //lives at /events/create
     @PostMapping("create")
     public String CreateEvent(@RequestParam String eventName){
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:";
     }
 }
