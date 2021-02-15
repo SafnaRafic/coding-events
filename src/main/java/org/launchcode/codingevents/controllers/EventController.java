7133ef53db3f13f2de91f2e754e5d8d40087d2cd
Safna <safna.usa@gmail.com>
2021-02-15 16:14:17 -0600

    Exercise updated

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,30 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events=new ArrayList<>();
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
-    public String displayAllEvents(Model model){
-        model.addAttribute("events",events);
+    public String displayAllEvents(Model model) {
+        HashMap<String, String> events = new HashMap<>();
+
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+
+        model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
-    @GetMapping("create")
-    public String renderCreateEventForm(){
-        return "events/create";
-    }
-
-    //lives at /events/create
-    @PostMapping("create")
-    public String CreateEvent(@RequestParam String eventName,@RequestParam String eventDescription){
-        events.add(new Event(eventName,eventDescription));
-        return "redirect:";
-    }
 }
 
